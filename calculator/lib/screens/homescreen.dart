be17235623e47755d083500e7025bf8cbe3d4959
MaviaZindani <maviazindaninfo@gmail.com>
2024-07-13 21:42:00 +0500

    Simple Calculatro

---
+++ calculator/lib/screens/homescreen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_simple_calculator/flutter_simple_calculator.dart';
+import 'package:math_expressions/math_expressions.dart';
+
+class homeScreen extends StatefulWidget {
+  const homeScreen({super.key});
+
+  @override
+  State<homeScreen> createState() => _homeScreenState();
+}
+
+class _homeScreenState extends State<homeScreen> {
+  var userinput = '';
+  var answer = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: SimpleCalculator(
+          theme: const CalculatorThemeData(
+            displayColor: Colors.black,
+            displayStyle: const TextStyle(fontSize: 80, color: Colors.yellow),
+            /*...*/
+          ),
+        ),
+      )
+    );
+  }
+
+  void equalpress() {
+    String firstUserInput = userinput;
+    firstUserInput = userinput.replaceAll('x', '*');
+    String secondUserInput = firstUserInput;
+    secondUserInput = firstUserInput.replaceAll('÷', '/');
+
+    Parser p = Parser();
+    Expression expression = p.parse(secondUserInput);
+    ContextModel contextModel = ContextModel();
+
+    double eval = expression.evaluate(EvaluationType.REAL, contextModel);
+    answer = eval.toString();
+  }
+
+  void persenttage() {
+    var ThirdUserInput = int.parse(userinput);
+    var value = ThirdUserInput / 100;
+
+    answer = value.toString();
+  }
+}
---
+++ calculator/lib/widget/button.dart
@@ -0,0 +1,59 @@
+import 'package:calculator/utlis/colors.dart';
+import 'package:flutter/material.dart';
+
+class Button extends StatelessWidget {
+  const Button({super.key, required this.titel, this.color=dimblackcolor,required this.onPress});
+    final String titel;
+    final Color color;
+    final VoidCallback onPress;
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(30),
+      onTap: onPress,
+      child: Container(
+        height: 68,
+        width: 68,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Center(
+            child: Text(
+          titel,
+          style: const TextStyle(color: Colors.white, fontSize: 25),
+        )),
+      ),
+    );
+  }
+}
+
+
+class Button2 extends StatelessWidget {
+ const Button2({super.key, required this.titel, this.color=dimblackcolor,required this.onPress});
+
+  final String titel;
+  final color;
+  final VoidCallback onPress;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      borderRadius: BorderRadius.circular(30),
+      onTap: onPress,
+      child: Container(
+        height: 68,
+        width: 170,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(30),
+        ),
+        child: Center(
+            child: Text(
+          titel,
+          style: const TextStyle(color: Colors.white, fontSize: 25),
+        )),
+      ),
+    );
+  }
+}
